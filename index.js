1bd8f6e425513c35918153c633b574b276254c30
Supiyasingh <supriyaexit@gmail.com>
2024-12-06 22:27:15 +0530

    Added login function

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ function signup(userName) {
   
   console.log(signup("john")); // User Already Registered, Please Login
   console.log(signup("alice")); // Signup Successful, Please Login
+
+
+  function login(userName, password) {
+    const existingUsers = ["john", "jane", "bob"];
+    if (existingUsers.includes(userName)) {
+      if (password === "Emp@123") {
+        return "Login Successful...";
+      } else {
+        return "Wrong Password....";
+      }
+    } else {
+      return "User Not Found, Please Signup";
+    }
+  }
+  
+  console.log(login("john", "Emp@123")); // Login Successful...
+  console.log(login("john", "wrongpassword")); // Wrong Password....
+  console.log(login("alice", "Emp@123")); // User Not Found, Please Signup
